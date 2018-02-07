4ba145bbf486aff24ce80063aafab0b3798106b6
Henry Gonzales <henry.b.gonzales@obf.ateneo.edu>
2018-02-07 19:56:24 +0800

    db masterpiece -bagel

--- sql/glocal.sql
+++ sql/glocal.sql
@@ -1,15 +1,13 @@
 
 CREATE TABLE engineer(
 	eng_id int PRIMARY KEY NOT NULL,
+	department varchar(50) NOT NULL,
 	first_name varchar(50) NOT NULL,
-	last_name varchar(50) NOT NULL,
-	department varchar(50) NOT NULL
+	last_name varchar(50) NOT NULL
 );
 
-CREATE TABLE products(
-	product_name varchar(50) PRIMARY KEY NOT NULL,
-	vendor varchar(50) NOT NULL,
-	category varchar(50) NOT NULL
+CREATE TABLE vendor(
+	principal varchar(50) unique NOT NULL
 );
 
 CREATE TABLE client(
@@ -17,58 +15,98 @@ CREATE TABLE client(
 	priority_client boolean NOT NULL, 
 	date_expired date NOT NULL,
 	active_contract boolean NOT NULL,
+	contact_email varchar(50),
+	contact_number int,
 	no_cases int NOT NULL
 );
 
-CREATE TABLE activities(
-	case_track varchar(50),
-	engineer_id int references engineer(eng_id) NOT NULL,
-	client varchar(50) NOT NULL,
-	date_of_work date NOT NULL,
-	activity varchar(100) NOT NULL,
-	score int NOT NULL
+CREATE TABLE products(
+	product_name varchar(50) PRIMARY KEY NOT NULL,
+	product_line varchar(50) unique NOT NULL,	
+	vendor varchar(50) references vendor(principal) NOT NULL,
+	category varchar(50) NOT NULL
+);
+
+CREATE TABLE license(
+	product varchar(50) references products(product_name) NOT NULL,
+	license varchar(200)
+);
+
+CREATE TABLE contact_person(
+	client varchar(50) references client(account_name) NOT NULL,
+	person_name varchar(50) unique NOT NULL
 );
 
 CREATE TABLE case_monitoring(
-	case_id serial PRIMARY KEY NOT NULL,
-    tracking_no varchar(50),
-	account varchar(50) references client(account_name) NOT NULL,
-	product_name varchar(50) references products(product_name) NOT NULL,
-	principal_involved varchar(50)  NOT NULL,
-	date_open date NOT NULL,
-	issue varchar(50) NOT NULL,  
-	date_closed date NOT NULL,
-	duration int NOT NULL,
-	severity int NOT NULL
+	glocal_id serial unique PRIMARY KEY NOT NULL,
+    vendor_case_id varchar(50),
+	date_id_created date NOT NULL, 
+	date_raised date NOT NULL,
+	case_title varchar(50) NOT NULL,
+	case_description varchar(100) NOT NULL,
+	severity int NOT NULL,
+	vendor varchar(50) NOT NULL,
+	customer varchar(50) references client(account_name) NOT NULL,
+	product_line varchar(50) references products(product_line) NOT NULL,
+	customer_name varchar(50) NOT NULL,
+	systems_engineer_lead varchar(50),
+	assigned_account_manager varchar(50) NOT NULL,
+	assigned_systems_engineer text[][] NOT NULL
 );
 
-INSERT INTO engineer(eng_id,first_name, last_name, department)
+CREATE TABLE activities(
+	tracking_no int references case_monitoring(glocal_id) NOT NULL,
+	time_in timestamp NOT NULL, 
+	time_out timestamp NOT NULL,
+	product_code varchar(50) references products(product_line) NOT NULL,
+	client varchar(50) NOT NULL,
+	contact_customer varchar(50) references contact_person(person_name) NOT NULL,
+	address_of_work varchar(50) NOT NULL, 
+	type_of_activity varchar(50) NOT NULL, 
+	purpose_of_visit varchar(50) NOT NULL,
+	activity_performed varchar(250) NOT NULL,
+	next_activity varchar(250) NOT NULL,
+	recommendations varchar(250),
+	engineer_name varchar(50),
+	score int NOT NULL
+);
+
+INSERT INTO engineer(eng_id, department, first_name, last_name)
 VALUES 
-(001,'John','Jenkins','Security'),
-(002,'Isaiah','Solomon','Availability');
+(001,'Security','John','Jenkins'),
+(002,'Availability','Isaiah','Solomon'),
+(003, 'Security','Aaron','Hernandez');
 
-INSERT INTO products(product_name, vendor, category)
+INSERT INTO vendor(principal)
 VALUES
-('Multi-Cloud','Veritas','Security'),
-('Secure Web Gateway','Symmantec','Security');
+('Symmantec'),
+('Veritas');
 
-INSERT INTO client(account_name, priority_client, date_expired, active_contract, no_cases)
-VALUES 
-('Unionbank', TRUE, '03/08/2019', TRUE, 1),
-('BPI',FALSE,'06/25/2018',TRUE,0);
-
-INSERT INTO activities(case_track, engineer_id,client, date_of_work, activity, score)
+INSERT INTO client(account_name, priority_client, date_expired, active_contract, contact_email, contact_number, no_cases)
 VALUES
-('81xveK',001, 'Unionbank','02/22/2017','troubleshoot',2),
-('81xveK',001, 'Unionbank','02/23/2017','update software',5),
-('96gysw',002, 'BPI','01/02/2017','troubleshoot',2),
-('96gysw',002, 'BPI','01/04/2017','install hotfix',5);
+('Unionbank', TRUE, '03/08/2019', TRUE, 'unionbank@gmail.com',854321, 1),
+('BPI',FALSE,'06/25/2018',TRUE, 'bpi@gmail.com',123456, 0);
 
-INSERT INTO case_monitoring(case_id, tracking_no, account, product_name, principal_involved, date_open, issue, date_closed, duration, severity)
+INSERT INTO products(product_name, product_line, vendor, category)
 VALUES
-(DEFAULT,'96gysw','BPI','Multi-Cloud','Veritas','01/02/2018','startup failed', '01/05/2018', 3, 2),
-(DEFAULT,'81xveK', 'Unionbank','Secure Web Gateway','Symmantec','02/22/2017','system down', '02/24/2017', 2, 3);
+('Multi-Cloud','SFVVR','Veritas','Security'),
+('Secure Web Gateway','JHTEY','Symmantec','Security');
 
+INSERT INTO contact_person(client, person_name)
+VALUES
+('BPI','John Vincent'),
+('Unionbank','John Karlo');
+
+
+INSERT INTO case_monitoring(glocal_id, vendor_case_id, date_id_created, date_raised, case_title, case_description, severity, vendor, customer, product_line, customer_name, systems_engineer_lead, assigned_account_manager, assigned_systems_engineer)
+VALUES
+(2018-001,'2018-100','01/08/2018','01/07/2018','Attend to failure of backup','troubleshoot',1,'Veritas','Unionbank','SFVVR','John Karlo Tabios','Jefferson','Mei',ARRAY[['John Jenkins'],['Isaiah Solomon']]),
+(2018-002,'2018-111','03/08/2018','03/07/2018','Fix server for backup','install updates',1,'Symmantec','BPI','JHTEY','John Vincent Agbayani','Jeffrey','Maan',ARRAY['Aaron Hernandez']);
+
+INSERT INTO activities(tracking_no, time_in, time_out, product_code, client,  contact_customer, address_of_work, type_of_activity, purpose_of_visit,activity_performed, next_activity, recommendations, engineer_name, score)
+VALUES
+(2018-001, '2018-01-08 12:24:00', '2018-01-08 15:05:00', 'SFVVR','Unionbank','John Karlo','unionbank','OnSite','troubleshoot','checked if modules are up to date','install updates','no recommendations','Jenkins', 5),
+(2018-002,'2018-03-08 13:32:00','2018-03-08 16:22:00', 'JHTEY','BPI', 'John Vincent', 'BPI','Remote','troubleshooting','check version of software','install updates',' no recommendations','Hernandez',4);
 
 
 
--- sql/queries.sql
+++ sql/queries.sql
@@ -5,22 +5,36 @@ createdb -U postgres -h localhost -p 5432 glocaldb
 psql -U postgres -h localhost -p 5432 glocaldb < glocal.sql
 psql -U postgres -h localhost -p 5432 glocaldb
 
--- shows case with activities done 
-SELECT tracking_no, account, product_name, date_of_work, activity 
-FROM activities a JOIN case_monitoring c 
-ON a.case_track = c.tracking_no 
-WHERE engineer_id = 001;
+-- shows case with activities done(shows engineers also)
+SELECT glocal_id, date_id_created, case_title, product_code, customer, assigned_systems_engineer, activity_performed, recommendations
+FROM case_monitoring c JOIN activities a 
+ON c. glocal_id = a.tracking_no 
+WHERE glocal_id = 2018-001;
 
 -- show engineer with activities done 
-SELECT engineer_id, first_name, last_name, activity, date_of_work, case_track as tracking_number,client, score
-FROM engineer e JOIN activities a 
-ON e.eng_id = a.engineer_id 
-WHERE last_name ='Solomon';
+SELECT last_name||', '||first_name as full_name, purpose_of_visit, activity_performed, score
+FROM engineer e join activities a 
+ON e.last_name = engineer_name
+WHERE last_name = 'Jenkins';
 
 --show list of products and their respective vendors
-SELECT product_name, vendor 
+SELECT product_name, product_line, vendor 
 FROM products;
 
 -- show clients and contract status
 SELECT account_name, priority_client, active_contract, no_cases
 FROM client;
+
+-- show products with cases and activities
+SELECT product_name, vendor, client, type_of_activity, activity_performed, engineer_name
+FROM products p JOIN activities a 
+ON p.product_line = a.product_code
+WHERE product_name = 'Multi-Cloud';
+
+
+
+
+
+
+
+
