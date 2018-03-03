c581e7de7dc1978b91f641474427417f18e67b75
Henry Gonzales <henry.b.gonzales@obf.ateneo.edu>
2018-03-03 18:39:23 +0800

    added support for cors

--- glocal-updated/app.js
+++ glocal-updated/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/', routes)
 
 
